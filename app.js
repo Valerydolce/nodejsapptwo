4d69a8cdf1e2ece33f9ba273577af3242803050b
Paul Valery Simo <valerydolce@gmail.com>
2025-01-13 18:35:19 -0600

    updated the app.js file

--- app.js
+++ app.js
@@ -3,7 +3,32 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/hello', (req, res) => {
-  res.send('Hello, World! Welcome to iQuant YouTube Channel.\n');
+  const fancy = req.query.fancy;
+
+  if (fancy) {
+    res.send(`
+      <html>
+        <head>
+          <style>
+            @keyframes fadeIn {
+              from { opacity: 0; }
+              to { opacity: 1; }
+            }
+            .fancy-text {
+              font-size: 24px;
+              color: #333;
+              animation: fadeIn 2s ease-in-out;
+            }
+          </style>
+        </head>
+        <body>
+          <div class="fancy-text">Hello, World! Welcome to YVALCORP IT Training Bootcamp.</div>
+        </body>
+      </html>
+    `);
+  } else {
+    res.send('Hello, World! YVALCORP IT.\n');
+  }
 });
 
 app.listen(port, () => {
