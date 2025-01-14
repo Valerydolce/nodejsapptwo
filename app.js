064a51dfe6c0cad07e049e78a7dd74fafff0b049
Paul Valery Simo <valerydolce@gmail.com>
2025-01-13 19:01:43 -0600

    to the default

--- app.js
+++ app.js
@@ -3,32 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/hello', (req, res) => {
-  const fancy = req.query.fancy;
-
-  if (fancy) {
-    res.send(`
-      <html>
-        <head>
-          <style>
-            @keyframes fadeIn {
-              from { opacity: 0; }
-              to { opacity: 1; }
-            }
-            .fancy-text {
-              font-size: 24px;
-              color: #333;
-              animation: fadeIn 2s ease-in-out;
-            }
-          </style>
-        </head>
-        <body>
-          <div class="fancy-text">Hello, World! Welcome to YVALCORP IT Training Bootcamp.</div>
-        </body>
-      </html>
-    `);
-  } else {
-    res.send('Hello, World! YVALCORP IT...\n');
-  }
+  res.send('Welcome to Yvalcorp IT....\n');
 });
 
 app.listen(port, () => {
